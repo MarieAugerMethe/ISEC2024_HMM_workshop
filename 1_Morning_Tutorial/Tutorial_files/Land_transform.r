606f1c899f8825abeb962784c14cac87d93408b6
Marie Auger-Methe <marie.augermethe@gmail.com>
2024-07-08 12:23:20 -0700

    Remove land building files

--- 1_Morning_Tutorial/Tutorial_files/Land_transform.r
+++
@@ -1,7 +0,0 @@
-# set up land
-land <- st_read("data/NorthernBaffin_UTM.shp")
-land.ll <- st_transform(land, crs = 4326)
-crs(land.ll)
-ext.gps <- ext(tracks_gps_sf) + 0.5
-land.ll <- st_crop(land.ll, st_bbox(ext.gps))
-st_write(land.ll, "NorthBaffin.shp")
